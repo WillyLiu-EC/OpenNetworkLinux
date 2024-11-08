fcadc03ce54c779fb9d5a737aa6586bcf16f2eeb
willy_liu <willy_liu@accton.com>
2024-11-08 18:27:54 +0800

    [ACCTON][AS5915-16X]  Add new platform
    CPU: Intel Atom Processor C3308, 1.6GHz
    
    sub-system:
    DDR SDRAM: 8GB x 1 2400MHz with ECC (SO-DIMM) DDR4
    SPI Flash (Boot): 16MB x 2
    M.2 SSD: 32GB MLC
    TPM: SLB9670XQ2.0FW7.63 INFINEON
    
    Management :
    UART RS232 console port (RJ45), Out-band Management Ethernet port (RJ45)
    
    Timing:
    GNSS GPS Receiver (SMB reserved), 1PPS IN and 1PPS OUT(SMB), 10MHz IN and 10MHz OUT (SMB), ToD PPS (RJ45)
    
    MAC:
    Broadcom BCM88273, 32Gbps full duplex switching
    
    Ethernet Ports:
    4x 10GE SFP+, 8x 1GE SFP and 4x 1GE RJ45
    
    CPLD:
    Altera 5M2210ZF324I5N (FBGA324)
    
    FPGA:
    Altera EP4CGX50CF23I7N (FBGA484)
    
    PCB:
    18 Layers, TU-863+ for Motherboard
    8 Layers, CPU board
    
    PSU:
    Max 180W output, DC to DC, 1+1 redundant load-sharing
    
    Signed-off-by: willy_liu <willy_liu@accton.com>

---
+++ packages/platforms/accton/x86-64/as5915-16x/onlp/builds/x86_64_accton_as5915_16x/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_accton_as5915_16x Autogeneration
+#
+###############################################################################
+x86_64_accton_as5915_16x_AUTO_DEFS := module/auto/x86_64_accton_as5915_16x.yml
+x86_64_accton_as5915_16x_AUTO_DIRS := module/inc/x86_64_accton_as5915_16x module/src
+include $(BUILDER)/auto.mk
+
---
+++ packages/platforms/accton/x86-64/as5915-16x/onlp/builds/x86_64_accton_as5915_16x/module/inc/x86_64_accton_as5915_16x/x86_64_accton_as5915_16x_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_accton_as5915_16x Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __x86_64_accton_as5915_16x_DOX_H__
+#define __x86_64_accton_as5915_16x_DOX_H__
+
+/**
+ * @defgroup x86_64_accton_as5915_16x x86_64_accton_as5915_16x - x86_64_accton_as5915_16x Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_accton_as5915_16x-x86_64_accton_as5915_16x Public Interface
+ * @defgroup x86_64_accton_as5915_16x-config Compile Time Configuration
+ * @defgroup x86_64_accton_as5915_16x-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __x86_64_accton_as5915_16x_DOX_H__ */
