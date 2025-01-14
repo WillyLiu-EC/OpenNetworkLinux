c9d3e8f0d9ef70e57ae6142e953a8c50bd7dbac3
Willy Liu <willy@accton.com>
2025-01-14 10:23:16 +0000

    [ACCTON][AS7515-24X] Add platform for kernel 6.1
    CPU: Intel Xeon Processor D-1527
    2.2 GHz / 4 core / Cache : 6 MB / TDP : 35 W
    BIOS: AMI AptioV BIOS
    MAC: Broadcom BCM88284, 1 pcs, 360G Integrated Packet Processor,
    Traffic Manager, and Single-Chip Device
    PCB: 20-Layers, TU TU883 for Mainboard
    16-Layers, TUC TU872LK for CPU module
    8-Layers, NP155F for FAN board
    8-Layers, Tg-150 for Power board
    4-Layers, NP155F for Back Plane board
    8-Layers, TU-862HF for Timing board
    RAM: 2 * DDR4 SODIMM socket (ECC / Up to 48GB 2133 MHz) Ethernet Management port: 1x RJ-45
    Ethernet Ports: 4x QSFP28 + 20x SFP28
    Console Port: 1x RJ-45
    Power Supply: 12V from PSU * 2pcs
    Cooling: 5 pcs of 12V fans, hot-swappable
    Dimension: 300 mm (D: Depth) x 438.4 mm (W: Width) x 43.7 mm (H: Height) FPGA: ALTERA 5CGXFC4C6F23I7N, (1pcs, FBGA484 package) CPLD: LATTICE LCMXO3LF-9400C (1 pcs, CABGA484 package) USB Port: 1x USB 3.0 type-C
    1PPS Input/Output Port: 1x SMB male connector
    10MHz Input/Output port: 1x SMB male connector
    TOD Port: 1x RJ-45
    GNSS Port: 1x GNSS antenna port
    Embedded Controller: IT8528
    
    Signed-off-by: Willy Liu <willy@accton.com>

--- packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
+++ packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
@@ -650,3 +650,11 @@ class OnlPlatformPortConfig_40x100_13x400_2x10(object):
 class OnlPlatformPortConfig_64x800_2x25(object):
     PORT_COUNT=66
     PORT_CONFIG="64x800 + 2x25"
+
+class OnlPlatformPortConfig_4x100_20x25(object):
+    PORT_COUNT=24
+    PORT_CONFIG="4x100 + 20x25"
+
+class OnlPlatformPortConfig_48x1_6x10(object):
+    PORT_COUNT=54
+    PORT_CONFIG="48x1 + 6x10"
