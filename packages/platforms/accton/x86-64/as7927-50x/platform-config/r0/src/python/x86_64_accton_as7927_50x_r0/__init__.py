0cb923c56c4e92b729fc4fe15dad0e0d6772a3bc
Willy Liu <willy@accton.com>
2025-10-15 09:45:32 +0000

    Enable Efuse for port 48 and modify config
    Signed-off-by: Willy Liu <willy@accton.com>

--- packages/platforms/accton/x86-64/as7927-50x/platform-config/r0/src/python/x86_64_accton_as7927_50x_r0/__init__.py
+++ packages/platforms/accton/x86-64/as7927-50x/platform-config/r0/src/python/x86_64_accton_as7927_50x_r0/__init__.py
@@ -88,7 +88,7 @@ class OnlPlatform_x86_64_accton_as7927_50x_r0(OnlPlatformAccton,
             self.insmod("x86-64-accton-as7927-50x-%s" % m)
 
         # initialize SFP devices
-        for port in range(1, 48):
+        for port in range(1, 49):
             subprocess.call('echo 1 > /sys/devices/platform/as7927_50x_fpga/module_efuse_%d' % (port), shell=True)
         for port in range(49, 51):
             subprocess.call('echo 1 > /sys/devices/platform/as7927_50x_fpga/module_enable_%d' % (port), shell=True)
